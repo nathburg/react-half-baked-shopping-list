bd28cd19038e65adaf67e9918508885eba2d3d8e
Nathan Burgess <nathans.email.address@gmail.com>
2022-09-28 15:43:46 -0700

    sign out works

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,28 @@
-import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { useContext, useState } from 'react';
+import { Link, Redirect } from 'react-router-dom';
+import { UserContext } from '../context/UserContext';
 
 import { signOut } from '../services/auth';
 
 export default function Header() {
   const [isActive, setIsActive] = useState(false);
   // TODO -- replace this user with the user from context
-  const user = { email: 'this is a placeholder' };
+  
+  const { user, setUser } = useContext(UserContext);
 
   const handleLogout = async () => {
     try {
       await signOut();
-      // TODO -- set the user to null after logout
+      setUser(null);
     } catch (e) {
       // eslint-disable-next-line no-console
       console.error(e.message);
     }
   };
+
+  if (!user) {
+    return <Redirect to='/auth/sign-in' />;
+  }
 
   return (
     <nav className="navbar is-success" role="navigation" aria-label="main navigation">
